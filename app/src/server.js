9d270d94b402e2ff13cadab3000c644faf9c0ba8
smilercompiler <183860441+Astroarkscope7@users.noreply.github.com>
2026-08-20 22:19:49 +0530

    Revert "Release intentionally broken DeployFlow 1.0.7"
    This reverts commit e765e43bfced22247b123f56b5c70bbd490ba76f.

--- app/src/server.js
+++ app/src/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const app = require("./app");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`DeployFlow running on port ${PORT}`);
